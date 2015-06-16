9e359158a9bd6dcbf05946afdb28c2fe9247961c
KonstantinLA <konstantinla87@gmail.com>
2015-06-17 00:28:56 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var salaries = {
+  "Вася": 100,
+  "Петя": 300,
+  "Даша": 250
+};
+
+var sum = 0;
+function empty() {
+  for (var key in salaries) {
+    
+  }
+  return "нет сотрудников";
+}
